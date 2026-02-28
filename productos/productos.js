3266818994c74a9b1ccd61f5e646af30c77b161d
Markentas <equipomarkentas@gmail.com>
2026-02-28 10:55:37 -0300

    Agrega section productos disponibles

---
+++ productos/productos.js
@@ -0,0 +1,5 @@
+const productos = [
+    { nombre: "Torta de Chocolate", imagen: "torta-chocolate.jpg", precio: "$3.500", disponible: 0 },
+    { nombre: "Cupcakes", imagen: "cupcakes.jpg", precio: "$2.800", disponible: 0 },
+    { nombre: "Rostizado", imagen: "rostizado.jpg", precio: "$4.200", disponible: 0 }
+];
